c8a407b526ec442939276c0bfddc8185185c41f8
abner-tech <2020151792@ub.edu.bz>
2024-11-01 15:06:44 -0600

    comments-adding

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,21 +15,33 @@ func (a *applicationDependences) routes() http.Handler {
 
 	//method 404
 	router.NotFound = http.HandlerFunc(a.notFoundResponse)
-
-	//setup routes for the products table database interaction
+	//helth check
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthChechHandler)
+
+	//setup routes for the products table d\atabase interaction
+	//display all products
 	router.HandlerFunc(http.MethodPost, "/v1/products", a.createProductHandler)
+	//display a specific product
 	router.HandlerFunc(http.MethodGet, "/v1/product/:pid", a.displayProductHandler)
+	//update a specific product
 	router.HandlerFunc(http.MethodPatch, "/v1/product/:pid", a.updateProductHandler)
+	//delette a specific product
 	router.HandlerFunc(http.MethodDelete, "/v1/product/:pid", a.deleteProductHandler)
+	//display all products--includes sorting, filetering and searching
 	router.HandlerFunc(http.MethodGet, "/v1/products", a.listProductHandler)
 
 	//setup routes for the reviews table database interactions
+	//create a review for a porduct using product id
 	router.HandlerFunc(http.MethodPost, "/v1/reviews/:pid", a.create_P_ReviewHandler)
+	//list a specific review for a product
 	router.HandlerFunc(http.MethodGet, "/v1/product/:pid/review/:rid", a.listSingleProductReviewHandler)
+	//update a specific review for a specific product
 	router.HandlerFunc(http.MethodPatch, "/v1/product/:pid/review/:rid", a.updateProductReviewByIDS_Handler)
+	//delete a review
 	router.HandlerFunc(http.MethodDelete, "/v1/product/:pid/review/:rid", a.deleteReviewByIDS_Handler)
+	//display all reviews
 	router.HandlerFunc(http.MethodGet, "/v1/reviews", a.listReviewHandler)
+	//display a specific review for a specific product
 	router.HandlerFunc(http.MethodGet, "/v1/prod/reviews/:pid", a.listReviewHandler)
 
 	return a.recoverPanic(router)
